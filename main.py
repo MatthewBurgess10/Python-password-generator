97956fa3512d8ed92e8352b2acea1ffe6a970e86
Matthew Burgess <matthew456burgess@btinternet.com>
2023-05-28 18:00:30 +0100

    Finished code

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
 import random
-
-lowercharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-                   "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-                   "u", "v", "w", "x", "y", "z"]
-uppercharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                  "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-                  "U", "V", "W", "X", "Y", "Z"]
-    
-    
+import string
 
 
+def generation(length):
+  characters = string.ascii_letters + string.digits + string.punctuation
+  password = ''.join(random.choice(characters)for i in range(length))
+  return password
 
-
-lowercharacternum = random.randint(1, 15)
-
-uppercharacternum = random.randint(1, 15)
-
-specialcharacternum = random.randint(1, 15)
-
-
-for i in range(0,lowercharacternum):
-  lowercharacterchoice = random.choice(str(lowercharacters))
-
-for i in range(0,uppercharacternum):
-  uppercharacterchoice = random.choice(str(uppercharacter))
-  
-#for i in range(0,specialcharacternum):
-  #specialcharacterchoice = random.choice(str(specialcharacter))
- 
-            
-             
-
-orderedpassword = lowercharacterchoice + uppercharacterchoice #+ specialcharacterchoice
-
-shuffledstring = ''.join(random.sample(orderedpassword, len(orderedpassword)))
-print(shuffledstring)
+length = int(input('Enter the length of the password: '))
+password = generation(length)
+print('Your password is {}'.format(password))
 
 
     
